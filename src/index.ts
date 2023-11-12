4748601fcacbf1f8ddde45417b78e73e446ca0de
Kerim Satirli <k.satirli@gmail.com>
2023-11-11 16:45:03 -0800

    adds docs and comments

--- src/index.ts
+++ src/index.ts
@@ -29,19 +29,33 @@ function createBot(): {
 export const { instance, bot } = createBot()
 
 bot.once('spawn', async () => {
+  // see https://www.npmjs.com/package/mineflayer-pathfinder
   bot.loadPlugin(pathfinder)
+
+  // see https://www.npmjs.com/package/mineflayer-auto-eat
   bot.loadPlugin(autoEat)
+
+  // see https://www.npmjs.com/package/mineflayer-tool
   bot.loadPlugin(tool)
+
+  // see https://www.npmjs.com/package/mineflayer-pvp
   bot.loadPlugin(pvp)
 
+  // see https://www.npmjs.com/package/mineflayer-auto-eat#botautoeatoptionspriority
   bot.autoEat.options.priority = instance.getSettings().hungerPriority
   bot.autoEat.options.startAt = instance.getSettings().hungerLimit
   bot.autoEat.options.bannedFood = instance.getSettings().hungerBannedFood
 
+  // see https://www.npmjs.com/package/mineflayer-pathfinder#movements
   bot.pathfinder.setMovements(new Movements(bot))
 
+  // disable building of 1x1 blocks when moving upwards
   bot.pathfinder.movements.allow1by1towers = false
+
+  // disable jumping over distances further than 1 block when moving
   bot.pathfinder.movements.allowParkour = false
+
+  // disable breaking of blocks when moving
   bot.pathfinder.movements.canDig = false
 
   instance.log.info(instance.locale.msg_greetings)
